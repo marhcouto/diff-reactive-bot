3ddfe33742adf49589005246086c8f3f6fefa48b
marhcouto <marhcouto1101@gmail.com>
2023-10-09 15:03:03 +0100

    feat: basic control

--- src/wall_following_diff_robot/__init__.py
+++ src/wall_following_diff_robot/__init__.py
@@ -19,57 +19,58 @@ class SerpController(Node):
 
         #!
         # Predefined speed for the robot
-        self.linear_speed = 0.5
+        self.linear_speed = 0.2
         self.angular_speed = 1.5
 
         #!
         # Goal distance from wall
         self.ideal_distance = 0.2
+
+        #!
+        # Goal angle with wall
+        self.ideal_angle = -math.pi / 2
         
         # **** Create publishers ****
-        self.pub: Publisher = self.create_publisher(Twist, "/cmd_vel", 1)
+        self.pub : Publisher = self.create_publisher(Twist, "/cmd_vel", 1)
         # ***************************
 
         # **** Create subscriptions ****
         self.create_subscription(LaserScan, "/static_laser", self.processLiDAR, 1)
 
 
-    # Send a request to move a model
-    def move_model(self, model_name, x, y, theta):
-        client = self.create_client(MoveModel, "/move_model")
-        client.wait_for_service()
-        request = MoveModel.Request()
-        request.name = model_name
-        request.pose = Pose2D()
-        request.pose.x = x
-        request.pose.y = y
-        request.pose.theta = theta
-        client.call_async(request)
-
-
     #! 
     # @brief Control the robot to follow the wall
     # @param publisher Publisher to publish Twist messages
-    # @param min_distance Minimum distance from the wall from a laser
+    # @param distance Minimum distance from the wall from a laser
     # @param angle_with_wall Angle with the wall
     # @return None
-    def controlRobot(self, publisher: Publisher, min_distance: float, angle_with_wall: float):
+    def controlRobot(self, publisher : Publisher, distance : float, angle_with_wall : float):
         # Control the robot to follow the wall
-        pass
+        distance_error : float = distance - self.ideal_distance
+        angle_error : float = angle_with_wall - self.ideal_angle
+        rclpy.logging.get_logger("SerpController").debug("Distance error: " + str(distance_error))
+        rclpy.logging.get_logger("SerpController").debug("Angle error: " + str(angle_error))
+        twist_msg : Twist = Twist()
+        k: float = 1 # Proportional constant
+        twist_msg.angular.z : float = angle_error * k
+        twist_msg.linear.x : float = self.linear_speed / (1 + abs(angle_error))
+        rclpy.logging.get_logger("SerpController").debug("Linear speed: " + str(twist_msg.linear.x))
+        rclpy.logging.get_logger("SerpController").debug("Angular speed: " + str(twist_msg.angular.z))
+        publisher.publish(twist_msg)
 
 
     #!
     # @brief Process LiDAR information
     # @param data LiDAR data
     # @return None
-    def processLiDAR(self, data: LaserScan):
+    def processLiDAR(self, data : LaserScan):
         numpy_ranges = np.array(data.ranges)
         number_of_lasers = len(numpy_ranges)
         numpy_ranges = np.vectorize(lambda x: x if x > 0 else 100) (numpy_ranges) 
         min_distance_measurement, min_distance_index = numpy_ranges.min(), numpy_ranges.argmin()
-        angle_with_wall = min_distance_index * data.angle_increment
-        rclpy.logging.get_logger("SerpController").info("Index: " + str(min_distance_index) + " of " + str(number_of_lasers))
-        rclpy.logging.get_logger("SerpController").info("Angle with wall: " + str(angle_with_wall * 180 / math.pi))
+        angle_with_wall = min_distance_index * data.angle_increment + data.angle_min
+        rclpy.logging.get_logger("SerpController").debug("Index: " + str(min_distance_index) + " of " + str(number_of_lasers))
+        rclpy.logging.get_logger("SerpController").debug("Angle with wall: " + str(angle_with_wall * 180 / math.pi))
         self.controlRobot(self.pub, min_distance_measurement, angle_with_wall)
 
 
