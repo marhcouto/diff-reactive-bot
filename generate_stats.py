7ac7337007f9e3ced3627b89831a8cb5252ab2cc
Bruno Mendes <bd_mendes@outlook.com>
2023-10-21 17:04:05 +0100

    Fix script

--- generate_stats.py
+++ generate_stats.py
@@ -8,13 +8,14 @@ import subprocess
 import matplotlib.pyplot as plt
 import numpy as np
 
-INITIAL_POSITION = [5.0, 4.0]
 
 def retrieve_stats():
     with open("log.txt") as f:
         travelled = float(f.readline().split(" ")[1])
+        mean_distance_to_wall = float(f.readline().split(" ")[1])
         elapsed = float(f.readline().split(" ")[1])
-        return {"travelled": travelled, "elapsed": elapsed}
+        return {"travelled": travelled, "mean_distance_to_wall": mean_distance_to_wall,
+                "elapsed": elapsed}
 
 
 def kill(process):
@@ -29,7 +30,10 @@ def kill(process):
     subprocess.run(['pkill', '-f', 'rviz'])
     time.sleep(1)
 
-async def run_simulation_and_read_log(pos_x=None, pos_y=None, target_velocity=None):
+
+async def run_simulation_and_read_log(pos_x=None, pos_y=None,
+                                      target_velocity=None, ideal_distance=None,
+                                      k_ang=None, k_lin=None, invert_direction=None):
     # delete log file
     file = Path("log.txt")
     if file.exists():
@@ -43,6 +47,14 @@ async def run_simulation_and_read_log(pos_x=None, pos_y=None, target_velocity=No
     cmd += "ros2 launch wall_following_diff_robot wall_following_diff_robot.launch.py"
     if target_velocity is not None:
         cmd += f" target_velocity:={target_velocity}"
+    if ideal_distance is not None:
+        cmd += f" ideal_distance:={ideal_distance}"
+    if k_ang is not None:
+        cmd += f" k_ang:={k_ang}"
+    if k_lin is not None:
+        cmd += f" k_lin:={k_lin}"
+    if invert_direction is not None:
+        cmd += f" invert_direction:={invert_direction}"
 
     process = await asyncio.create_subprocess_shell(cmd)
     time.sleep(1)
@@ -53,44 +65,37 @@ async def run_simulation_and_read_log(pos_x=None, pos_y=None, target_velocity=No
             kill(process)
             return retrieve_stats()
 
-def dump_graphics(full_stats):
-    def variable_graphic(filtered_stats, variable_name):
-        values = set([stat[variable_name] for stat in filtered_stats])
-        for value in values:
-            filtered_stats = [stat for stat in filtered_stats if stat[variable_name] == value]
-            print(filtered_stats)
-            positions = [str((stat["offset_x"], stat["offset_y"])) for stat in filtered_stats]
-
-            # scatter plot with (offset_x, offset_t) as x axis and elapsed as y axis
-            plt.scatter(positions, [stat["elapsed"] for stat in filtered_stats])
-            plt.title("Elapsed time from different initial positions")
-            plt.savefig(f"elapsed_{variable_name}_{value}.png")
-            plt.close()
-
-            # scatter plot with (offset_x, offset_t) as x axis and travelled as y axis
-            plt.scatter(positions, [stat["travelled"] for stat in filtered_stats])
-            plt.title("Travelled distance from different initial positions")
-            plt.savefig(f"travelled_{variable_name}_{value}.png")
-            plt.close()
-
-    variable_graphic(full_stats, "target_velocity")
-
 
 async def main():
+    INITIAL_POSITION = [5.0, 4.0]
     stats = []
 
-    for offset_x in range(-2,0):
-        for offset_y in range(-3,-1):
-             for target_velocity in range(4, 5):
-                stat = await run_simulation_and_read_log(INITIAL_POSITION[0] + offset_x/2,
-                                                           INITIAL_POSITION[1] + offset_y/2, target_velocity/10)
-                stat["offset_x"] = offset_x
-                stat["offset_y"] = offset_y
-                stat["target_velocity"] = target_velocity
-                stats.append(stat)
-
-    dump_graphics(stats)
-
+    # independent variables
+    for offset_x in range(-2, 0):
+        for offset_y in range(-3, -1):
+            for invert_direction in [True, False]:
+                for target_velocity in [0.3, 0.5]:
+                    for ideal_distance in [0.5, 0.8, 1.0]:
+                        for k_ang in [8.0, 16.0]:
+                            for k_lin in [1.0, 2.0]:
+                                stat = await run_simulation_and_read_log(INITIAL_POSITION[0] + offset_x/2,
+                                                                         INITIAL_POSITION[1] + offset_y/2, target_velocity,
+                                                                         ideal_distance, k_ang, k_lin, invert_direction)
+                                stat["offset_x"] = offset_x
+                                stat["offset_y"] = offset_y
+                                stat["target_velocity"] = target_velocity
+                                stat["ideal_distance"] = ideal_distance
+                                stat["invert_direction"] = invert_direction
+                                stat["k_ang"] = k_ang
+                                stat["k_lin"] = k_lin
+                                stats.append(stat)
+                                break
+                            break
+                        break
+                    break
+                break
+            break
+        break
 
 if __name__ == "__main__":
     asyncio.run(main())
